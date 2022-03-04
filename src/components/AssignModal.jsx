56fc08d0b134903f8716e6b661b8b81b3ad3cc8f
Vidmis <v.gutauskas90@gmail.com>
2022-03-04 22:08:37 +0200

    labels

--- src/components/AssignModal.jsx
+++ src/components/AssignModal.jsx
@@ -12,20 +12,48 @@ const AssingModal = (props) => {
     }
   };
 
+  const renderCategories = (categories, group = [], level = 0) => {
+    if (Array.isArray(categories)) {
+      return categories.map((category, catIndex) => {
+        const { id, name, sub_categories } = category;
+
+        const nextGroup = level == 0 ? [id] : [...group, id];
+
+        return (
+          <div key={`cat_${level}_${catIndex}_${nextGroup.join('_')}`} className="my-2 ml-2">
+            <div className="flex flex-row justify-between items-center">
+              <label htmlFor={category.name} className="flex-1 flex mr-4">
+                <p>{name}</p>
+              </label>
+              <input id={category.name} type="checkbox" className="h-7 w-7 rounded-full"></input>
+            </div>
+            {!!sub_categories && renderCategories(sub_categories, nextGroup, level + 1)}
+          </div>
+        );
+      });
+    }
+
+    return null;
+  };
+
   return (
     <>
       <div
         onClick={closeModal}
-        className="backdrop absolute top-0 left-0 bg-zinc-700 flex flex-col justify-center items-center bg-opacity-40 w-full h-screen"
+        className="backdrop absolute top-0 left-0 bg-zinc-700 flex flex-col justify-center items-center bg-opacity-40 w-full h-screen mt-14"
       >
-        <div className="flex flex-row w-1/2 h-1/2 bg-zinc-700 rounded-lg items-center justify-center text-white">
-          <div>
+        <div className="flex flex-row w-4/5 h-1/2 bg-zinc-700 rounded-lg items-center justify-center text-white">
+          {/* <div>
             <ul>
               {categories?.map((category) => (
-                <li>{category.name}</li>
+                <li>
+                  <label htmlFor={category.name}>{category.name}</label>
+                  <input id={category.name} type="checkbox" />
+                </li>
               ))}
             </ul>
-          </div>
+          </div> */}
+          {renderCategories(categories)}
         </div>
         <button className="text-white p-2 bg-cyan-500" onClick={() => props.setIsOpen(false)}>
           Close
